ff0ef6f9c6cc13e5ba13c1a5cb49d8fa8038d49e
J D <jd@Js-MacBook-Pro.ht.home>
2023-03-13 20:46:48 -0400

    Added fib logic

--- flitton_fib_py/__init__.py
+++ flitton_fib_py/__init__.py
@@ -1,2 +1,13 @@
+from typing import Optional
+
+def recurring_fibonacci_number(number: int) -> Optional[int]:
+    if number < 0:
+        return None
+    elif number <= 1:
+        return number
+    else:
+        return recurring_fibonacci_number(number - 1) + \
+            recurring_fibonacci_number(number - 2)
+
 def say_hello() -> None:
     print("the Flitton Fibonacci module is saying hello")
