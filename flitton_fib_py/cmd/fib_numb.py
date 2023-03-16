90e703433d10539d0de92e4e43f5966141b0399d
J D <jd@Js-MacBook-Pro.ht.home>
2023-03-15 20:26:16 -0400

    Added cli function

--- flitton_fib_py/cmd/fib_numb.py
+++ flitton_fib_py/cmd/fib_numb.py
@@ -7,7 +7,7 @@ def fib_numb() -> None:
         description='Calculate Fibonacci numbers'
     )
 
-    parser.add_argument('--number', action='store', type=int, require=True, help="Fibanocci number to be calculated")
+    parser.add_argument('--number', action='store', type=int, required=True, help="Fibanocci number to be calculated")
 
     arg = parser.parse_args()
     print(f"Your fibonacci number is: " f"{recurring_fibonacci_number(number=arg.number)}")
