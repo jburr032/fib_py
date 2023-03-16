23fa6a34f362fe531e1888fbd02d48ac621375a4
J D <jd@Js-MacBook-Pro.ht.home>
2023-03-15 21:28:21 -0400

    Rust wrapper

--- setup.py
+++ setup.py
@@ -1,29 +1,31 @@
+#!/usr/bin/env python
+from setuptools import dist
+
+dist.Distribution().fetch_build_eg-gs(['setuptools_rust'])
+
 from setuptools import find_packages, setup
+from setuptools_rust import Binding, RustExtension
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
     name="flitton_fib_py",
-    version="0.0.1",
-    author="Maxwell Flitton",
-    author_email="maxwell@gmail.com",
-    description="Calculates a Fibonacci number",
-    long_description="A basic library that caluclates fib numbers",
-    long_description_content_type="text/markdown",
-    url="",
-    install_requires=[],
-    packages=find_packages(exclude=("tests",)),
-    classifier=[
-        "Development Status :: 4 - Beta",
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
+    version="0.1",
+    rust_extensions=[RustExtension(
+        ".flitton_fib_rs.flitton_fib_rs",
+        path="Cargo.toml",
+        binding=Binding.PyO3
+    )],
+    packages=["flitton_fib_rs"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Rust",
+        "Operating System :: POSIX",
+        "Operating System :: MacOS :: MacOS X",
     ],
-    python_requires='>=3',
-    tests_require=['pytest'],
-    entry_points={
-        'console_scripts': [
-            'fib-number = flitton_fib_py.cmd.fib_numb:fib_numb'
-        ]
-    }
+    zip_safe=False,
 )
